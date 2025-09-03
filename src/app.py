e2d0a4d9b60a54fbd6b8872dd23756c85c5a5a62
giorgoskokkinos <kokkinos1@gmail.com>
2025-09-04 00:41:43 +0200

    Redirect user to spotify auth screen

--- src/app.py
+++ src/app.py
@@ -1,16 +1,17 @@
 import os
 import requests
-from flask import Flask,jsonify,request 
-from spotify_api_comm import spotify_access_token
+from flask import Flask,jsonify,request,redirect
+from spotify_api_comm import spotify_access_token,authorize_user_request
+
 app = Flask(__name__)
 
 @app.route('/')
 def home():
     return jsonify({"displayed_message": f"New Journey!!! Backend Working..."} )
 
-@app.route('/api/login', methods = ['POST'])
-def login():
-    return jsonify({"message": "{Login successful!}"})
+# @app.route('/api/login', methods = ['POST'])
+# def login():
+#     return jsonify({"message": "{Login successful!}"})
 
 
 @app.route('/api/artists/<artist_id>',methods = ['GET'])
@@ -21,7 +22,11 @@ def get_artist(artist_id):
     request= requests.get(url = url,headers= headers)
     return request.json()
 
+@app.route('/api/login', methods=['GET'])
+def login():
+    auth_url = authorize_user_request()
+    return redirect(auth_url)
 
-
-
+# if __name__ == '__main__':
+#     app.run(debug=True, port=5000)
 
--- src/spotify_api_comm.py
+++ src/spotify_api_comm.py
@@ -1,19 +1,32 @@
-
 import os
 import requests
+import urllib.parse
+CLIENT_ID = os.getenv('CLIENT_ID')
+CLIENT_SECRET = os.getenv('CLIENT_SECRET')
+
 
 def spotify_access_token():
-    client_id = os.getenv('CLIENT_ID')
-    client_secret = os.getenv('CLIENT_SECRET')
+    
     url = 'https://accounts.spotify.com/api/token'
     headers = {
         'Content-Type': 'application/x-www-form-urlencoded',
     }
     body={
         'grant_type': 'client_credentials',
-        'client_id': client_id,
-        'client_secret': client_secret
+        'client_id': CLIENT_ID,
+        'client_secret': CLIENT_SECRET
     }
     r = requests.post(url, headers=headers, data=body)
     print(type(r.json()))
     return r.json()['access_token']
+
+def authorize_user_request():
+    url = 'https://accounts.spotify.com/authorize'
+    params = {
+        'client_id': CLIENT_ID,
+        'redirect_uri': 'http://127.0.0.1:5000/api/callback',
+        'response_type': 'code',
+        'scope': 'playlist-modify-private playlist-modify-public'
+    }
+    auth_url = f"{url}?{urllib.parse.urlencode(params)}"
+    return auth_url
