d55a0c36709305a22e11aa1aff8292b74208a60b
giorgoskokkinos <kokkinos1@gmail.com>
2025-09-06 00:11:25 +0200

    Implement full OAuth 2.0 code flow
    -Create /api/login route to redirect users to Spotify for authorization
    
    - create /api/callback route to handle redirect from Spotify API
    
    -Add logic for exchaning authorization code for access token
    
    - Store received token to user's session.

--- src/app.py
+++ src/app.py
@@ -1,13 +1,15 @@
 import os
 import requests
-from flask import Flask,jsonify,request,redirect
-from spotify_api_comm import spotify_access_token,authorize_user_request
+from flask import Flask,jsonify,request,redirect,render_template ,session
+from spotify_api_comm import spotify_client_access_token,authorize_user_request,request_api_token_request
 
 app = Flask(__name__)
 
+app.secret_key = os.getenv('SECRET_KEY')
+
 @app.route('/')
 def home():
-    return jsonify({"displayed_message": f"New Journey!!! Backend Working..."} )
+    return render_template('basic_login.html')
 
 # @app.route('/api/login', methods = ['POST'])
 # def login():
@@ -16,7 +18,7 @@ def home():
 
 @app.route('/api/artists/<artist_id>',methods = ['GET'])
 def get_artist(artist_id):
-    access_token = spotify_access_token()
+    access_token = spotify_client_access_token()
     url = f'https://api.spotify.com/v1/artists/{artist_id}'
     headers = {"Authorization": f"Bearer {access_token}"}
     request= requests.get(url = url,headers= headers)
@@ -30,3 +32,23 @@ def login():
 # if __name__ == '__main__':
 #     app.run(debug=True, port=5000)
 
+@app.route('/api/callback')
+def callback():
+    error = request.args.get('error')
+    state = request.args.get('state')
+    code = request.args.get('code')
+    if error:
+         return render_template('error.html')
+    elif code:
+        req = request_api_token_request(code=code,redirect_uri='http://127.0.0.1:5000/api/callback')
+        session['token_obj'] = {
+            'access_token' : req.get('access_token'),
+            'expires_in' : req.get('expires_in'),
+            'scope' : req.get('scope')
+        }
+        
+        return jsonify('Stored token :', session['token_obj'])
+    
+    
+        
+       
--- src/spotify_api_comm.py
+++ src/spotify_api_comm.py
@@ -1,11 +1,12 @@
 import os
 import requests
 import urllib.parse
+import base64
 CLIENT_ID = os.getenv('CLIENT_ID')
 CLIENT_SECRET = os.getenv('CLIENT_SECRET')
 
 
-def spotify_access_token():
+def spotify_client_access_token():
     
     url = 'https://accounts.spotify.com/api/token'
     headers = {
@@ -16,9 +17,8 @@ def spotify_access_token():
         'client_id': CLIENT_ID,
         'client_secret': CLIENT_SECRET
     }
-    r = requests.post(url, headers=headers, data=body)
-    print(type(r.json()))
-    return r.json()['access_token']
+    req = requests.post(url, headers=headers, data=body)
+    return req.json()['access_token']
 
 def authorize_user_request():
     url = 'https://accounts.spotify.com/authorize'
@@ -30,3 +30,22 @@ def authorize_user_request():
     }
     auth_url = f"{url}?{urllib.parse.urlencode(params)}"
     return auth_url
+
+def request_api_token_request(code : str , redirect_uri : str):
+    auth_string = f'{CLIENT_ID}:{CLIENT_SECRET}'
+    print(auth_string)
+    auth_bytes = auth_string.encode('utf-8')
+    auth_base64 = base64.b64encode(auth_bytes).decode('utf-8')
+    url = 'https://accounts.spotify.com/api/token'
+    headers = {
+        'Authorization': f'Basic {auth_base64}',
+        'Content-Type' : 'application/x-www-form-urlencoded' 
+
+    }
+    body = {
+        'grant_type' : 'authorization_code',
+        'code' : code,
+        'redirect_uri' : redirect_uri
+    }
+    req = requests.post(url=url,headers=headers,data=body)
+    return req.json()
