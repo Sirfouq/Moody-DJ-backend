785b45e97cea3514c9b0e8a3268d44d99bb3c68b
giorgoskokkinos <kokkinos1@gmail.com>
2025-10-07 02:48:07 +0200

    -modify CORS parameters to allow communication with frontend -Minor property naming modification

--- src/app.py
+++ src/app.py
@@ -6,9 +6,13 @@ from flask_cors import CORS
 from spotify_api_comm import spotify_client_access_token,authorize_user_request,request_api_token_request,refresh_api_token_request
 
 app = Flask(__name__)
-CORS(app=app)
+CORS(app=app,
+     supports_credentials=True,
+     origins=['http://127.0.0.1:5173'],)
 
 app.secret_key = os.getenv('SECRET_KEY')
+
+
 
 @app.route('/')
 def home():
@@ -21,7 +25,7 @@ def home():
 def status_check():
     token_info = session.get('token_info')
     if not token_info:
-        return redirect(url_for('home'))
+        return jsonify({"isLoggedIn": False})
     now = time.time()
     is_expired = token_info.get('expires_at',0) < now
     if(is_expired):
@@ -34,14 +38,12 @@ def status_check():
             'scope' : refresh_token_request.get('scope'),
             'refresh_token': refresh_token_request.get('refresh_token') or token_info.get('refresh_token')
             }
-            return jsonify({'message': 'Token has been refreshed successfully.'})
+            return jsonify({"isLoggedIn": True})
+            
         except:
             session.clear()
-            return render_template('error.html')
-    return jsonify({'status':'200','message':'You are logged in and your token is valid!'})
-            
-
-    
+            return jsonify({"isLoggedIn": False})
+    return jsonify({"isLoggedIn": True})    
 
 
 
@@ -76,7 +78,7 @@ def callback():
             'refresh_token' : req.get('refresh_token')
         }
         print({'Stored token' : session['token_info']})
-        return redirect(url_for('status_check'))
+        return redirect('http://127.0.0.1:5173')
     
 if __name__ == '__main__':
     app.run(debug=True, host='127.0.0.1', port=5000)    
